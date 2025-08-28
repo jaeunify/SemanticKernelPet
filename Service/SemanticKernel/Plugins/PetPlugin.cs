6c28255ac5fcbf812fa138d87dd2380d678a7794
com2us-jaeeun <jaeeun.lee.4h@gmail.com>
2025-08-28 16:59:08 +0900

    pet plugin 추가 (근데 실행을 안함 ..)

---
+++ Service/SemanticKernel/Plugins/PetPlugin.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel;
+using Microsoft.SemanticKernel;
+
+namespace SemanticKernelService.Plugins
+{
+    public class PetPlugin
+    {
+        [KernelFunction, Description("Called when the pet is angry.")]
+        public string Angry([Description("The reason why the pet is angry")] string reason)
+        {
+            return $"[The pet is angry. Reason: {reason}]";
+        }
+
+        [KernelFunction, Description("Called when the pet gives a present to the user.")]
+        public string GivePresent([Description("The present the pet is giving")] string present)
+        {
+            return $"[The pet gives a present. Present: {present}]";
+        }
+
+        [KernelFunction, Description("Called when default")]
+        public string Default()
+        {
+            return $"[The pet is Default]";
+        }
+    }
+}
